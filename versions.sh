713b0bf799adca9132c6c38430f8186f676725d4
Lyra Naeseth <lyra@medium.com>
2017-10-04 17:31:46 -0700

    Fix versions.sh

--- versions.sh
+++ versions.sh
@@ -1,7 +1,7 @@
 #!bash
 
-[[ -n "$NGINX" ]] || NGINX_V='1.13.5'
-[[ -n "$OPENRESTY" ]] || OPENRESTY_V='1.11.2.4'
+[[ -n "$NGINX_V" ]] || NGINX_V='1.13.5'
+[[ -n "$OPENRESTY_V" ]] || OPENRESTY_V='1.11.2.4'
 
 readonly OPENSSL_V='1.0.2k'
 readonly PCRE_V='8.21'
